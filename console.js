58380c4c32f1ad44121609680a742e34c8c83c28
gh-canon <ryan@antisanity.net>
2018-10-30 19:06:08 -0500

    Bug-fix.
    Fixed a function rendering bug.

--- console.js
+++ console.js
@@ -279,8 +279,9 @@
                 .replace(/[/][/].*$/mg, '') // strip single-line comments
                 .replace(/\s+/g, '') // strip white space
                 .replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments  
-                .split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters  
-                .replace(/=[^,]+/g, '') // strip any ES6 defaults  
+                .split(/(\)?=>)|\){/, 1)[0]
+                .replace(/^[^(]*[(]/, '') // extract the parameters  
+                .replace(/=[^,\)]+/g, '') // strip any ES6 defaults  
                 .split(',').filter(Boolean); // split & filter [""]
         }
 
